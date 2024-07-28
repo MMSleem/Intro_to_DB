5db9d3f0868c142b0ac522766d86d89231f7e605
MMSleem <cptmsleem@gmail.com>
2024-07-28 23:36:05 +0300

    New chngs

--- task_4.sql
+++ task_4.sql
@@ -1,14 +1,11 @@
--- Select the database to use
-USE alx_book_store;
-
--- Retrieve full description of the 'books' table from the information_schema
+-- Use the information_schema to retrieve full description of the 'books' table
 SELECT
     COLUMN_NAME AS 'Column Name',
     COLUMN_TYPE AS 'Column Type',
     IS_NULLABLE AS 'Is Nullable',
     COLUMN_DEFAULT AS 'Default Value',
     EXTRA AS 'Extra Information'
-FROM information_schema.COLUMNS
+FROM INFORMATION_SCHEMA.COLUMNS
 WHERE
-    TABLE_SCHEMA = DATABASE()
+    TABLE_SCHEMA = 'alx_book_store'
     AND TABLE_NAME = 'Books';
