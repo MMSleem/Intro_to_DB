de652fbb28b46a052a859dfe1c2dfa2dc5b5725a
MMSleem <cptmsleem@gmail.com>
2024-07-28 22:37:44 +0300

    Committed new Changes

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,10 +1,15 @@
--- Create the 'Authors' table
+-- Create the database
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+
+USE alx_book_store;
+
+-- Create the Authors table
 CREATE TABLE Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
 
--- Create the 'Books' table
+-- Create the Books table
 CREATE TABLE Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
@@ -14,23 +19,23 @@ CREATE TABLE Books (
     FOREIGN KEY (author_id) REFERENCES Authors (author_id)
 );
 
--- Create the 'Customers' table
+-- Create the Customers table
 CREATE TABLE Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
-    email VARCHAR(215) NOT NULL UNIQUE,
+    email VARCHAR(215) NOT NULL,
     address TEXT
 );
 
--- Create the 'Orders' table
+-- Create the Orders table
 CREATE TABLE Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
-    order_date DATE NOT NULL,
+    order_date DATE,
     FOREIGN KEY (customer_id) REFERENCES Customers (customer_id)
 );
 
--- Create the 'Order_Details' table
+-- Create the Order_Details table
 CREATE TABLE Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
