4776f42c1647ed73c53837f30a9ad846abbcb6a9
MMSleem <cptmsleem@gmail.com>
2024-07-28 23:09:55 +0300

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,17 +1,12 @@
 -- Drop existing tables if they exist
 DROP TABLE IF EXISTS Order_Details;
-
 DROP TABLE IF EXISTS Orders;
-
 DROP TABLE IF EXISTS Books;
-
 DROP TABLE IF EXISTS Customers;
-
 DROP TABLE IF EXISTS Authors;
 
 -- Create the database
 CREATE DATABASE IF NOT EXISTS alx_book_store;
-
 USE alx_book_store;
 
 -- Create the Authors table
@@ -27,7 +22,7 @@ CREATE TABLE Books (
     author_id INT,
     price DOUBLE NOT NULL,
     publication_date DATE,
-    FOREIGN KEY (author_id) REFERENCES Authors (author_id)
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
 -- Create the Customers table
@@ -43,7 +38,7 @@ CREATE TABLE Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES Customers (customer_id) ON DELETE CASCADE
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id) ON DELETE CASCADE
 );
 
 -- Create the Order_Details table
@@ -52,6 +47,6 @@ CREATE TABLE Order_Details (
     order_id INT,
     book_id INT,
     quantity DOUBLE NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES Orders (order_id) ON DELETE CASCADE,
-    FOREIGN KEY (book_id) REFERENCES Books (book_id) ON DELETE CASCADE
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id) ON DELETE CASCADE,
+    FOREIGN KEY (book_id) REFERENCES Books(book_id) ON DELETE CASCADE
 );
